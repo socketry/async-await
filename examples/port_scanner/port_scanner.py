941eb9a1bdbd47bad8d0b7c5ce749002b526b8e4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-06-10 18:42:09 +1200

    Use getrlimit to detect open file limit, and use semaphore to guard against too many open files.

--- examples/port_scanner/port_scanner.py
+++ examples/port_scanner/port_scanner.py
@@ -1,28 +1,28 @@
-import os
+#!/usr/bin/env python
+
+import os, resource
 import asyncio
 
 class PortScanner:
     def __init__(self, host="0.0.0.0", ports=range(1, 1024+1)):
         self.host       = host
         self.ports      = ports
-        self.open_files = 0
-        self.file_limit = int(os.popen("ulimit -n").read())
+        limits          = resource.getrlimit(resource.RLIMIT_NOFILE)
+        self.semaphore  = asyncio.Semaphore(value=limits[0])
         self.loop       = asyncio.get_event_loop()
 
     async def scan_port(self, port, timeout=0.5):
-        try:
-            if self.open_files >= self.file_limit:
-                raise OSError
-            fut = asyncio.open_connection(self.host, port, loop=self.loop)
-            self.open_files += 1
-            await asyncio.wait_for(fut, timeout=0.5)
-            print("{} open".format(port))
-        except (asyncio.TimeoutError, ConnectionRefusedError):
-            print("{} closed".format(port))
-            self.open_files -= 1
-        except OSError:
-            asyncio.sleep(timeout)
-            asyncio.gather(self.scan_port(port, timeout))
+        async with self.semaphore:
+            try:
+                future = asyncio.open_connection(self.host, port, loop=self.loop)
+                reader, writer = await asyncio.wait_for(future, timeout=0.5)
+                print("{} open".format(port))
+                reader.close()
+                writer.close()
+            except ConnectionRefusedError:
+                print("{} closed")
+            except asyncio.TimeoutError:
+                print("{} timeout".format(port))
 
     def start(self, timeout=0.5):
         self.loop.run_until_complete(asyncio.gather(
@@ -30,6 +30,6 @@ class PortScanner:
             ))
 
 
-scanner = PortScanner(ports=range(1, 1024+1))
+scanner = PortScanner(ports=range(1, 65536))
 
 scanner.start()
